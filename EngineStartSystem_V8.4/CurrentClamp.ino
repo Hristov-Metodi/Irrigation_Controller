774d9605d15a919086855de7d866830b5671c21c
Hristov-Metodi <hristov.metodi@gmail.com>
2017-09-04 22:09:07 +0300

    Add files via upload

--- EngineStartSystem_V8.4/CurrentClamp.ino
+++ EngineStartSystem_V8.4/CurrentClamp.ino
@@ -2,19 +2,25 @@
 boolean WellPumpCurrentClampCheck()
 {
   Log("WellPumpCurrentClampCheck BEGIN");
-  
-  WellPumpIrmsNew = WellPumpCurrentClamp.calcIrms(1480);  // Calculate Irms only
 
+  delay(100);
+  WellPumpIrmsNew = (WellPumpCurrentClamp.calcIrms(100))/10;  // Calculate Irms only (1480)
+  delay(50);
+  
   Log("Well Pump Current = " + String(WellPumpIrmsNew) + " A");
   
-  if(WellPumpIrmsNew >= (WellPumpIrmsOld/2))
+  if(WellPumpIrmsNew >= (WellPumpIrmsOld/4))    // Detecst a 4X drop in the current 
   {
         Log("WELL PUMP IS WORKING!");
         WellPumpIrmsOld = WellPumpIrmsNew;
         return 1; 
   }
+  else
+  {
+    Log("WELL PUMP IS IDLE!");
+    WellPumpIrmsOld = 0;
+    return 0;
+  }
  
-  Log("WELL PUMP IS IDLE!");
-  WellPumpIrmsOld = 0;
-  return 0;
+
 }
